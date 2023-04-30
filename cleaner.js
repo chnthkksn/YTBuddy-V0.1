b345aabf6aec52fe9f3689f47f5b4050880541d7
chnthkksn <chnthkksn@gmail.com>
2023-04-30 16:07:09 +0530

    edited cleaner.js to check if files are there

--- cleaner.js
+++ cleaner.js
@@ -5,32 +5,34 @@ const jetpack = require('fs-jetpack');
 const path = require('path');
 
 function cleaner(type) {
-    if (type === 'audio') {
-        var uploadsDir = __dirname + '/public/downloads/audio/' 
-    }
-    if (type === 'video') {
-        var uploadsDir = __dirname + '/public/downloads/video/' 
-    }
+  if (type === 'audio') {
+    var uploadsDir = __dirname + '/public/downloads/audio/'
+  }
+  if (type === 'video') {
+    var uploadsDir = __dirname + '/public/downloads/video/'
+  }
 
-    fs.readdir(uploadsDir, function(err, files) {
-        files.forEach(function(file, index) {
-          fs.stat(path.join(uploadsDir, file), function(err, stat) {
-            var endTime, now;
-            if (err) {
-              return console.error(err);
-            }
-            now = new Date().getTime();
-            endTime = new Date(stat.ctime).getTime() + 1800000;
-            if (now > endTime) {
-                return jetpack.removeAsync(path.join(uploadsDir, file)).then(function() {
-                    return console.log('successfully deleted');
-                }).catch(function(err) {
-                    return console.error('error occurred: ' + err);
-                });
-            }
-          });
+  if (fs.existsSync(uploadsDir)) {
+    fs.readdir(uploadsDir, function (err, files) {
+      files.forEach(function (file, index) {
+        fs.stat(path.join(uploadsDir, file), function (err, stat) {
+          var endTime, now;
+          if (err) {
+            return console.error(err);
+          }
+          now = new Date().getTime();
+          endTime = new Date(stat.ctime).getTime() + 900000;
+          if (now > endTime) {
+            return jetpack.removeAsync(path.join(uploadsDir, file)).then(function () {
+              return console.log('successfully deleted');
+            }).catch(function (err) {
+              return console.error('error occurred: ' + err);
+            });
+          }
         });
       });
+    });
+  }
 }
 
 module.exports = cleaner;
