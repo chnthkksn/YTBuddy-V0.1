7943a282ef6f33ac455ed1be74d499f21c2feb6a
chnthkksn <chnthkksn@gmail.com>
2023-04-30 16:26:47 +0530

    modified:   cleaner.js

--- cleaner.js
+++ cleaner.js
@@ -13,23 +13,28 @@ function cleaner(type) {
   }
 
   fs.readdir(uploadsDir, function (err, files) {
-    files.forEach(function (file, index) {
-      fs.stat(path.join(uploadsDir, file), function (err, stat) {
-        var endTime, now;
-        if (err) {
-          return console.error(err);
-        }
-        now = new Date().getTime();
-        endTime = new Date(stat.ctime).getTime() + 900000;
-        if (now > endTime) {
-          return jetpack.removeAsync(path.join(uploadsDir, file)).then(function () {
-            return console.log('successfully deleted');
-          }).catch(function (err) {
-            return console.error('error occurred: ' + err);
-          });
-        }
+    if (files !== undefined) {
+      files.forEach(function (file, index) {
+        fs.stat(path.join(uploadsDir, file), function (err, stat) {
+          var endTime, now;
+          if (err) {
+            return console.error(err);
+          }
+          now = new Date().getTime();
+          endTime = new Date(stat.ctime).getTime() + 900000;
+          if (now > endTime) {
+            return jetpack.removeAsync(path.join(uploadsDir, file)).then(function () {
+              return console.log('successfully deleted');
+            }).catch(function (err) {
+              return console.error('error occurred: ' + err);
+            });
+          }
+        });
       });
-    });
+    }
+    else {
+      console.log('No files to delete');
+    }
   });
 }
 
